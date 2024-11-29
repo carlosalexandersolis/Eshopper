b110608bd0d35bff2b4d03469d61578c81d1defa
carlosalexandersolis <alexandersolismarrufo@gmail.com>
2024-11-29 16:06:54 -0600

    Add files via upload

--- ticket.php
+++ ticket.php
@@ -1,4 +1,7 @@
 <?php 
+session_start();
+$usuario = $_SESSION["usuario"];
+$email = $_SESSION["usuario"];
    require('rounded_rect.php');
    setlocale(LC_TIME, "Spanish_Mexican");
 
@@ -13,11 +16,14 @@
    $pdf->Image('../Ticket.png',10,5,180,300);
    $pdf->Ln(45);
    $pdf->Cell(80,10,date("Y-m-d"),0,1,'R');
+   $pdf->Cell(100,40,$usuario,0,1,'R');
+   
     if(file_exists('../carritodecompras.txt')){
       $content = trim(file_get_contents('../carritodecompras.txt'), PHP_EOL);
       $lineas = explode(PHP_EOL, $content);
       $total = 0;
       $pdf->Cell(30,70,' ', 0,1, 'R');
+        $total = 0;
 
       foreach($lineas as $linea){
          list($productoE, $precioE) = explode(',', $linea);
@@ -25,7 +31,7 @@
          $pdf->Cell(160,-10,"$ " . $precioE, 0,1, 'R');
          $pdf->Cell(30,10,' ', 0,1, 'R');
          $total = $total + $precioE;
-      }
+      $pdf->cell(30,30´´,0,1´R´);
    $pdf->Ln(93);
    $pdf->Cell(160,10,"$ " . $total, 0,1, 'R');
    $pdf->Output();
